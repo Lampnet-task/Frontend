cd800d4bfe7ed188146ab1fb9a96423bb851938c
Dayo Farayola <dayojnr.3@gmail.com>
2024-02-26 13:47:08 +0100

    feat: structuring app

--- wazobia-foods/src/App.jsx
+++ wazobia-foods/src/App.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Welcome home!</h1>
+      <Outlet />
     </>
   )
 }
--- wazobia-foods/src/main.jsx
+++ wazobia-foods/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const route = createBrowserRouter([
+    {
+      path: "/",
+      element:  <App />,
+      // children: []
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={route} />
   </React.StrictMode>,
 )
