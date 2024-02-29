62b03ec50f59bbcba91c08388a1fe18a968d3f0a
Dayo Farayola <dayojnr.3@gmail.com>
2024-02-29 17:18:38 +0100

    fix: product styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,9 @@
-
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
-import ProductList from './Components/Products/ProductList';
-import ProductDetails from './Components/Products/ProductDetails';
-import OrderCart from './Components/Products/OrderCart';
 import { Outlet } from 'react-router-dom'
 import Header from "../src/Components/Header"
 import Footer from "../src/Components/Footer"
 
 function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path='/' element={<Home />} />
-//         <Route path='/products/:productId' element={<ProductDetails />} />
-//         <Route path='/orders' element={<OrderCart />} />
-//       <Route path='*' element={<h1 className='h-screen flex justify-center items-center text-white text-6xl'>PAGE NOT FOUND</h1>} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// function Home() {
-//   return (
-//     <>
-//       <div className='bg-blue-950'>
-//         <ProductList className='products' />
-//       </div>
-//     </>
-//   );
-// =======
     return (
         <>
             <Header />
