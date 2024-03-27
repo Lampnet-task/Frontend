1aa75ef8ea7b6c4cdce2c5fb04ffddc87e88a714
NwabuezeM <122814541+NwabuezeM@users.noreply.github.com>
2024-03-27 23:34:21 +0100

    ,

--- src/Components/Products/ProductCard.jsx
+++ src/Components/Products/ProductCard.jsx
@@ -54,7 +54,7 @@ const ProductCard = ({ product }) => {
       {/* Product title */}
       <h2 className="font-bold my-2 px-4 text-3xl text-cyan-400">{title}</h2>
       {/* Product description */}
-      <h3 className='text-2xl mb-3'>{description.length > 40 ? description.slice(0, 60).concat('...') : description}</h3>
+      <h3 className='text-2xl mb-3'>{description.length > 80 ? description.slice(0, 80).concat('...') : description}</h3>
       {/* Star ratings */}
       <div className="flex">{starRating(ratings)}</div>
       {/* Product price */}
